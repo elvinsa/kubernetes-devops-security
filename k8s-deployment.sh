28436982b3c037b76ada23539d0711194c3c6284
elvinsa <47449713+elvinsa@users.noreply.github.com>
2023-09-17 19:04:58 +0400

    updated

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,5 +2,5 @@
 
 #k8s-deployment.sh
 
-sed -i 's#replace#replace#elvinsa/numeric-app#g' k8s_deployment_service.yaml
-kubectl -n default apply -f k8s_deployment_service.yaml
+sed -i 's#replace#replace#${imageName}#g' k8s_deployment_service.yaml
+kubectl -n default get deployment ${deploymenName} > /dev/null
