e9d3c02452884ec28f54a80029e8b5897eec8923
elvinsa <47449713+elvinsa@users.noreply.github.com>
2023-09-17 19:09:20 +0400

    try
    try again

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,4 +3,4 @@
 #k8s-deployment.sh
 
 sed -i 's#replace#replace#${imageName}#g' k8s_deployment_service.yaml
-kubectl -n default get deployment ${deploymenName} > /dev/null
+kubectl -n default get deployment ${deploymentName} > /dev/null
